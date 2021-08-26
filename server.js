721b03e0d1b6e979dc600e600a4d26946ff05e9a
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2021-08-26 19:43:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(request, response){
-  response.send("<h1>Hello world</h1>");
+app.get("/", function(req, res){
+  res.send("<h1>Hello world</h1>");
 })
 
 app.listen(3000, function(){
